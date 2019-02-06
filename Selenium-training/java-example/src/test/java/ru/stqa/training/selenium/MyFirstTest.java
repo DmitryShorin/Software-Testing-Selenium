b65c059b6730a415dd23f23fe10c49bf2088e635
Dmitriy Shorin <tombstone@bk.ru>
2019-02-06 23:20:10 +0300

    изменен исходник задания

--- Selenium-training/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ Selenium-training/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -3,12 +3,9 @@ package ru.stqa.training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
     public WebDriver driver;
@@ -23,9 +20,6 @@ public class MyFirstTest {
     @Test
     public void myFirstTest() {
         driver.navigate().to("http://www.google.com");
-/*        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));*/
     }
 
     @After
