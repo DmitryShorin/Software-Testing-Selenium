f18e836864cb1f13ee9142a97a144ef659ba0e68
Dmitriy Shorin <tombstone@bk.ru>
2019-02-07 22:23:53 +0300

    Добавлено Лекция 2 Задача 3

---
+++ Selenium-training/java-example/src/test/java/ru/stqa/training/selenium/TestLecture2Task3.java
@@ -0,0 +1,38 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestLecture2Task3 {
+    // Лекция 2, Задание 3. Сделайте сценарий логина
+
+    // Инициализируем и настраиваем драйвер
+    public WebDriver driver;
+    public WebDriverWait wait;
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+    // Описываем сценарий для логина в панель администрирования учебного приложения
+    // http://localhost/litecart/admin/
+    @Test
+    public void Login() {
+        final String inputText = "admin";
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys(inputText);
+        driver.findElement(By.name("password")).sendKeys(inputText);
+        driver.findElement(By.name("login")).click();
+    }
+    // Останавливаем браузер
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
