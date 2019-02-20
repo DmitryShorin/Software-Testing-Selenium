4c02dd35b0da5c1d10cd1151e911d0cd7d2cf4a1
Dmitriy Shorin <tombstone@bk.ru>
2019-02-20 20:01:30 +0300

    + в 8 задании улучшен локатор

--- Selenium-training/java-example/src/test/java/ru/stqa/training/selenium/Task8.java
+++ Selenium-training/java-example/src/test/java/ru/stqa/training/selenium/Task8.java
@@ -14,8 +14,8 @@ public class Task8 extends TestBase {
     public void CheckSticker() {
         webDriver.navigate().to("http://localhost/litecart/en/");
         // Получаем список товаров
-        //List<WebElement> items = webDriver.findElements(By.cssSelector("div.content .link"));
-        List<WebElement> items = webDriver.findElements(By.cssSelector("div.content li.product.column.shadow.hover-light"));
+        //List<WebElement> items = webDriver.findElements(By.cssSelector("div.content li.product.column.shadow.hover-light"));
+        List<WebElement> items = webDriver.findElements(By.cssSelector(".product"));
         // Проверяем кол-во стикеров у товара
         for (int i = 0; i < items.size(); i++) {
             List<WebElement> stickers = items.get(i).findElements(By.cssSelector("[class^=sticker]"));
