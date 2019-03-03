ff1329d5a9128a2eaab46d619403c33138860a7b
Dmitriy Shorin <tombstone@bk.ru>
2019-03-03 19:05:36 +0300

    + задание 15

---
+++ Selenium-training/java-example/src/test/java/ru/stqa/training/selenium/Task15.java
@@ -0,0 +1,29 @@
+package ru.stqa.training.selenium;
+
+
+import org.junit.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Task15 {
+
+    @Test
+    public void Test() throws MalformedURLException {
+        final URL LOCALHOST = new URL("http://localhost:4444/wd/hub");
+
+        //WebDriver webDriver = new RemoteWebDriver(LOCALHOST, new InternetExplorerOptions().ignoreZoomSettings());
+        WebDriver webDriver = new RemoteWebDriver(LOCALHOST, new ChromeOptions());
+
+        webDriver.navigate().to("http://google.com");
+        webDriver.quit();
+    }
+}
